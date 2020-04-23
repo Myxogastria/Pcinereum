53b37afca6d08c527c9a6ba874b845dcefbf7067
Myxogastria <50288647+Myxogastria@users.noreply.github.com>
2020-04-24 03:20:02 +0900

    add ChatStringBuilder

--- app/src/main/java/physarum/cinereum/MainActivity.kt
+++ app/src/main/java/physarum/cinereum/MainActivity.kt
@@ -7,20 +7,42 @@ import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private var chatStringBuilder = ChatStringBuilder()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val bt1 = findViewById<Button>(R.id.button1)
-        val listener = ButtonListener()
-        bt1.setOnClickListener(listener)
+        val bt2 = findViewById<Button>(R.id.button2)
+        val bt3 = findViewById<Button>(R.id.button3)
+        val bt4 = findViewById<Button>(R.id.button4)
+
+        bt1.setOnClickListener(ButtonListener(0))
+        bt2.setOnClickListener(ButtonListener(1))
+        bt3.setOnClickListener(ButtonListener(2))
+        bt4.setOnClickListener(ButtonListener(3))
     }
 
-    private inner class ButtonListener : View.OnClickListener{
+    private inner class ButtonListener(val btId: Int) : View.OnClickListener{
         override fun onClick(v: View?) {
+            chatStringBuilder.send("Alice", btId)
             val output = findViewById<TextView>(R.id.tvChat)
-            output.text = output.text.toString() + "\nAlice: Hello"
+            output.text = chatStringBuilder.getString("US")
+        }
+    }
+
+    private inner class ChatStringBuilder{
+        var chatString: String
+        init {
+            chatString = ""
+        }
+        fun send(sender: String, phraseId: Int){
+            chatString += "\n$sender: $phraseId"
+        }
+
+        fun getString(lang: String): String{
+            return chatString
         }
     }
 }
