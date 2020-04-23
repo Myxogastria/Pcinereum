42b195825b065e7b84848af59708aca20db325f9
Myxogastria <50288647+Myxogastria@users.noreply.github.com>
2020-04-24 02:27:58 +0900

    first implementation of chat button

--- app/src/main/java/physarum/cinereum/MainActivity.kt
+++ app/src/main/java/physarum/cinereum/MainActivity.kt
@@ -2,11 +2,25 @@ package physarum.cinereum
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bt1 = findViewById<Button>(R.id.button1)
+        val listener = ButtonListener()
+        bt1.setOnClickListener(listener)
+    }
+
+    private inner class ButtonListener : View.OnClickListener{
+        override fun onClick(v: View?) {
+            val output = findViewById<TextView>(R.id.tvChat)
+            output.text = output.text.toString() + "\nAlice: Hello"
+        }
     }
 }
